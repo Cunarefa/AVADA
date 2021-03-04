68500dce165af6a562e3e7bafb575ece4be849e9
Yevgen <yevgen.yelik@gmail.com>
2021-03-05 00:53:33 +0200

    path for create/update

--- Cinema/admin_app/urls.py
+++ Cinema/admin_app/urls.py
@@ -6,7 +6,8 @@ from .views import *
 urlpatterns = [
     path('', admin_app, name='admin'),
     path('movies/', AdminAllMoviesList.as_view(), name='admin_movies'),
-    path('movie/create/', AdminCreateMovie.as_view(), name='add_movie'),
+    path('movie/create/', create_movie, name='add_movie'),
+    # path('movie/create/', AdminCreateMovie.as_view(), name='add_movie'),
     path('movie/update/<str:slug>/', AdminUpdateMovie.as_view(), name='update_movie'),
     path('movie/delete/<str:slug>/', AdminDeleteMovie.as_view(), name='delete_movie'),
 ]
