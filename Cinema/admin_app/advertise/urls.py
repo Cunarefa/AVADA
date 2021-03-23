63994ce02d0d6fbcb9e62f595c4a3b4155d16b93
Yevgen <yevgen.yelik@gmail.com>
2021-03-23 21:18:04 +0200

    add Views and URLs

--- Cinema/admin_app/advertise/urls.py
+++ Cinema/admin_app/advertise/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from admin_app.kids_room.views import AdminKidsRoomListView, CreateKidsRoomAdmin, DetailKidsRoomAdmin
+from admin_app.advertise.views import AdminAdvertiseListView, CreateAdvertiseAdmin, DetailAdvertiseAdmin
 
 urlpatterns = [
-    path('kids_rooms/', AdminKidsRoomListView.as_view(), name='kids_rooms_list'),
-    path('kids_room/create/', CreateKidsRoomAdmin.as_view(), name='create_kids_room'),
-    path('kids_room/<int:pk>/detail/', DetailKidsRoomAdmin.as_view(), name='kids_room_detail'),
+    path('advertise/', AdminAdvertiseListView.as_view(), name='advertise_list'),
+    path('advertise/create/', CreateAdvertiseAdmin.as_view(), name='create_advertise'),
+    path('advertise/<int:pk>/detail/', DetailAdvertiseAdmin.as_view(), name='advertise_detail'),
 ]
--- Cinema/admin_app/advertise/views.py
+++ Cinema/admin_app/advertise/views.py
@@ -4,54 +4,54 @@ from django.views.generic import ListView
 from extra_views import InlineFormSetFactory, CreateWithInlinesView, UpdateWithInlinesView
 
 from admin_app.news.forms import NewsAdminForm
-from movie_app.models import ChildrenRoom, ChildrenRoomGallery
+from movie_app.models import Advertise, AdvertiseGallery
 
 
-class AdminKidsRoomListView(ListView):
-    model = ChildrenRoom
-    template_name = 'kids_room/kids_rooms_list.html'
-    context_object_name = 'kids_rooms'
+class AdminAdvertiseListView(ListView):
+    model = Advertise
+    template_name = 'advertise/advertise_list.html'
+    context_object_name = 'advertise'
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        ctx = super(AdminKidsRoomListView, self).get_context_data(**kwargs)
-        ctx['title'] = 'Детские комнаты'
+        ctx = super(AdminAdvertiseListView, self).get_context_data(**kwargs)
+        ctx['title'] = 'Реклама'
         return ctx
 
 
 class GalleryInline(InlineFormSetFactory):
-    model = ChildrenRoomGallery
+    model = AdvertiseGallery
     fields = ['image']
     factory_kwargs = {'max_num': 3}
 
 
-class CreateKidsRoomAdmin(CreateWithInlinesView):
-    model = ChildrenRoom
+class CreateAdvertiseAdmin(CreateWithInlinesView):
+    model = Advertise
     inlines = [GalleryInline]
     fields = '__all__'
     template_name = 'news/create_news.html'
-    extra_context = {'title': 'Создать Детскую комнату'}
-    success_url = reverse_lazy('kids_rooms_list')
+    extra_context = {'title': 'Создать рекламу'}
+    success_url = reverse_lazy('advertise_list')
 
 
-class DetailKidsRoomAdmin(UpdateWithInlinesView):
-    model = ChildrenRoom
+class DetailAdvertiseAdmin(UpdateWithInlinesView):
+    model = Advertise
     inlines = [GalleryInline]
     fields = '__all__'
     template_name = 'news/update_news.html'
 
     def get_context_data(self, **kwargs):
-        ctx = super(DetailKidsRoomAdmin, self).get_context_data(**kwargs)
-        ctx['title'] = ChildrenRoom.objects.get(pk=self.kwargs['pk'])
+        ctx = super(DetailAdvertiseAdmin, self).get_context_data(**kwargs)
+        ctx['title'] = Advertise.objects.get(pk=self.kwargs['pk'])
         return ctx
 
     def form_valid(self, request):
         form = NewsAdminForm(self.request.POST, self.request.FILES, instance=self.object)
         formset = GalleryInline(self.request.POST, self.request.FILES, instance=self.object)
         if 'Удалить' in self.request.POST:
-            cafe_delete = ChildrenRoom.objects.get(pk=self.kwargs['pk'])
+            cafe_delete = Advertise.objects.get(pk=self.kwargs['pk'])
             cafe_delete.delete()
         else:
             self.object = form.save()
             formset.instance = self.object
             self.object = request.save()
-        return HttpResponseRedirect(reverse_lazy('kids_rooms_list'))
+        return HttpResponseRedirect(reverse_lazy('advertise_list'))
