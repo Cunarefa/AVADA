4bbbe753d041dee55e803d44bf10ab5127afbaa3
Yevgen <yevgen.yelik@gmail.com>
2021-03-23 21:16:14 +0200

    add urls.py views.py

--- Cinema/admin_app/action/urls.py
+++ Cinema/admin_app/action/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from admin_app.news.views import AdminNewsListView, CreateNewsAdmin, DetailNewsAdmin
+from admin_app.action.views import AdminActionListView, CreateActionAdmin, DetailActionAdmin
 
 urlpatterns = [
-    path('news/', AdminNewsListView.as_view(), name='news'),
-    path('news/create/', CreateNewsAdmin.as_view(), name='create_news'),
-    path('news/<int:pk>/detail/', DetailNewsAdmin.as_view(), name='detail_news'),
-    # path('news/delete/<int:pk>/', DeleteHall.as_view(), name='delete_news'),
+    path('actions/', AdminActionListView.as_view(), name='actions'),
+    path('action/create/', CreateActionAdmin.as_view(), name='create_action'),
+    path('action/<int:pk>/detail/', DetailActionAdmin.as_view(), name='action_detail'),
 ]
--- Cinema/admin_app/action/views.py
+++ Cinema/admin_app/action/views.py
@@ -4,55 +4,54 @@ from django.views.generic import ListView
 from extra_views import InlineFormSetFactory, CreateWithInlinesView, UpdateWithInlinesView
 
 from admin_app.news.forms import NewsAdminForm
-from movie_app.models import News, NewsGallery
+from movie_app.models import ActionGallery, Action
 
 
-class AdminNewsListView(ListView):
-    model = News
-    template_name = 'news/news_list.html'
-    context_object_name = 'news'
+class AdminActionListView(ListView):
+    model = Action
+    template_name = 'actions/actions_list.html'
+    context_object_name = 'actions'
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        ctx = super(AdminNewsListView, self).get_context_data(**kwargs)
-        ctx['title'] = 'Новости'
+        ctx = super(AdminActionListView, self).get_context_data(**kwargs)
+        ctx['title'] = 'Акции'
         return ctx
 
 
 class GalleryInline(InlineFormSetFactory):
-    model = NewsGallery
+    model = ActionGallery
     fields = ['image']
     factory_kwargs = {'max_num': 3}
 
 
-class CreateNewsAdmin(CreateWithInlinesView):
-    model = News
+class CreateActionAdmin(CreateWithInlinesView):
+    model = Action
     inlines = [GalleryInline]
     fields = '__all__'
     template_name = 'news/create_news.html'
-    extra_context = {'title': 'Создать новость'}
-    success_url = reverse_lazy('news')
+    extra_context = {'title': 'Создать акцию'}
+    success_url = reverse_lazy('actions')
 
 
-class DetailNewsAdmin(UpdateWithInlinesView):
-    model = News
+class DetailActionAdmin(UpdateWithInlinesView):
+    model = Action
     inlines = [GalleryInline]
     fields = '__all__'
     template_name = 'news/update_news.html'
-    success_url = reverse_lazy('news')
 
     def get_context_data(self, **kwargs):
-        ctx = super(DetailNewsAdmin, self).get_context_data(**kwargs)
-        ctx['title'] = News.objects.get(pk=self.kwargs['pk'])
+        ctx = super(DetailActionAdmin, self).get_context_data(**kwargs)
+        ctx['title'] = Action.objects.get(pk=self.kwargs['pk'])
         return ctx
 
     def form_valid(self, request):
         form = NewsAdminForm(self.request.POST, self.request.FILES, instance=self.object)
         formset = GalleryInline(self.request.POST, self.request.FILES, instance=self.object)
         if 'Удалить' in self.request.POST:
-            cinema_delete = News.objects.get(pk=self.kwargs['pk'])
+            cinema_delete = Action.objects.get(pk=self.kwargs['pk'])
             cinema_delete.delete()
         else:
             self.object = form.save()
             formset.instance = self.object
             self.object = request.save()
-        return HttpResponseRedirect(reverse_lazy('news'))
+        return HttpResponseRedirect(reverse_lazy('actions'))
