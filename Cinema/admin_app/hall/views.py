2b61cb64bcab40fe22bb3eef2eda66b6c786cdb3
Yevgen <yevgen.yelik@gmail.com>
2021-03-29 20:50:05 +0300

    add def form_valid to UpdateView

--- Cinema/admin_app/hall/views.py
+++ Cinema/admin_app/hall/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy
 from django.views.generic import DeleteView
 from extra_views import InlineFormSetFactory, CreateWithInlinesView, UpdateWithInlinesView
 
-from admin_app.hall.forms import HallForm, HallGalleryFormset
+from admin_app.hall.forms import HallForm
 from movie_app.models import Hall, HallGallery
 
 
@@ -21,11 +21,6 @@ class CreateHallView(CreateWithInlinesView):
     extra_context = {'title': 'Создать новый зал'}
     success_url = reverse_lazy('cinemas')
 
-    def get_context_data(self, **kwargs):
-        ctx = super(CreateHallView, self).get_context_data(**kwargs)
-        ctx['title'] = 'Создать новый зал'
-        return ctx
-
 
 class UpdateHallView(UpdateWithInlinesView):
     model = Hall
@@ -38,6 +33,18 @@ class UpdateHallView(UpdateWithInlinesView):
         ctx = super(UpdateHallView, self).get_context_data(**kwargs)
         ctx['title'] = Hall.objects.get(pk=self.kwargs['pk'])
         return ctx
+
+    def form_valid(self, request):
+        form = HallForm(self.request.POST, self.request.FILES, instance=self.object)
+        formset = GalleryInline(self.request.POST, self.request.FILES, instance=self.object)
+        if 'Удалить' in self.request.POST:
+            hall_delete = Hall.objects.get(pk=self.kwargs['pk'])
+            hall_delete.delete()
+        else:
+            self.object = form.save()
+            formset.instance = self.object
+            self.object = request.save()
+        return HttpResponseRedirect(reverse_lazy('cinemas'))
 
 
 class DeleteHall(DeleteView):
